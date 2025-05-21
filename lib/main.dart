9f652dc42869ecf79fa480feac916e8e6a38d538
Fiindah <fiendah24@gmail.com>
2025-05-21 10:10:54 +0700

    tugas dua flutter

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:belajar_flutter/meet_1/tugas1_flutter.dart';
 import 'package:belajar_flutter/meet_2/meet_2.dart';
+import 'package:belajar_flutter/meet_2/meet_2_a.dart';
+import 'package:belajar_flutter/meet_2/tugas2_flutter.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -33,7 +35,7 @@ class MyApp extends StatelessWidget {
         // tested with just a hot reload.
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
-      home: UserProfile(),
+      home: ProfilePage(),
     );
   }
 }
---
+++ lib/meet_2/tugas2_flutter.dart
@@ -0,0 +1,134 @@
+import 'package:flutter/material.dart';
+
+class ProfilePage extends StatelessWidget {
+  const ProfilePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.indigo,
+        title: Text("Profil Lengkap", style: TextStyle(color: Colors.white),),
+        centerTitle: true,
+        actions: [
+          IconButton(
+            icon: Icon(Icons.more_vert, color: Colors.white),
+            onPressed: () {
+              // Action for search button
+            },
+          ),
+        ],
+      ),
+     body: Column(
+        children: [
+          Expanded(
+            child: Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                   CircleAvatar(
+                    radius: 60.0,
+                    backgroundImage: AssetImage('assets/images/foto.jpg')),
+                 SizedBox(height: 20.0),
+                 Text('Aisyah Maharani', // Ganti dengan nama lengkap Anda
+                    style: TextStyle(
+                      fontSize: 24.0,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.blueAccent,
+                      fontFamily: 'RobotoMono',
+                      letterSpacing: 1.2,
+                    ),
+                    textAlign: TextAlign.center,
+                  ),
+                  SizedBox(height: 20.0),
+                  Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 20.0),
+                    child: Container(
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(10.0),
+                        color: Colors.grey[200],
+                      ),
+                      padding: EdgeInsets.all(10.0),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Icon(Icons.email, color: Colors.grey),
+                          SizedBox(width: 10.0),
+                          Text(
+                            'aisyah@email.com',
+                            style: TextStyle(fontSize: 16.0),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                  SizedBox(height: 10.0),
+                  Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 20.0),
+                    child: Row(
+                      children: [
+                        Spacer(),
+                        Icon(Icons.phone, color: Colors.grey),
+                        SizedBox(width: 10.0),
+                        Text(
+                          '0812-3456-7890',
+                          style: TextStyle(fontSize: 16.0),
+                        ),
+                      ],
+                    ),
+                  ),
+                  SizedBox(height: 20.0),
+                  Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 20.0),
+                    child: Row(
+                      children: [
+                        Expanded(
+                          child: Container(
+                            padding: EdgeInsets.all(15.0),
+                            color: Colors.blue[100],
+                            child: Center(
+                              child: Text(
+                                "Postingan",
+                                style: TextStyle(fontWeight: FontWeight.bold),
+                              ),
+                            ),
+                          ),
+                        ),
+                        SizedBox(width: 10.0),
+                        Expanded(
+                          child: Container(
+                            padding:EdgeInsets.all(15.0),
+                            color: Colors.green[100],
+                            child: Center(
+                              child: Text(
+                                "Followers",
+                                style: TextStyle(fontWeight: FontWeight.bold),
+                              ),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                   SizedBox(height: 30.0),
+                  Padding(
+                    padding: EdgeInsets.all(20.0),
+                    child: Text(
+                      "Seorang Mobile Programming pemula menyukai tantangan dan ingin terus berkembang. Saat ini sedang belajar di PPKD Jakarta Pusat.",
+                      textAlign: TextAlign.center,
+                      style: TextStyle(fontSize: 16.0),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+          Container(
+            height: 50.0,
+            color: Colors.amber[300],
+          ),
+        ],
+      ),
+    );
+  }
+}
