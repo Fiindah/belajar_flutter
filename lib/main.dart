c1da64516ea7131aecf3728932fad8efab105e23
Fiindah <fiendah24@gmail.com>
2025-05-24 09:35:11 +0700

    materi meet lima

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:belajar_flutter/meet_4/tugas3_flutter.dart';
+import 'package:belajar_flutter/meet_5/tugas5_flutter.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -32,7 +32,7 @@ class MyApp extends StatelessWidget {
         // tested with just a hot reload.
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
-      home: TugasTiga(),
+      home: TugasLima(),
     );
   }
 }
