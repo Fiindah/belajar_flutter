0b1e35318510776388ee84468adc908b6fe091d8
Fiindah <fiendah24@gmail.com>
2025-05-26 11:47:05 +0700

    tugas enam flutter

---
+++ lib/meet_6/tugas6_flutter.dart
@@ -0,0 +1,280 @@
+import 'package:flutter/material.dart';
+
+class TugasEnam extends StatefulWidget {
+  const TugasEnam({super.key});
+
+  @override
+  State<TugasEnam> createState() => _TugasEnamState();
+}
+
+class _TugasEnamState extends State<TugasEnam> {
+  final TextEditingController passwordController = TextEditingController();
+  bool _obscureText = true;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color(0xff00224F),
+      appBar: AppBar(
+        backgroundColor: Color(0xff00224f),
+        leading: Icon(Icons.arrow_back_ios, color: Colors.white),
+        centerTitle: true,
+        title: Text(
+          "Login",
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 18,
+            fontFamily: 'Roboto',
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      ),
+
+      body: Column(
+        children: [
+          const SizedBox(height: 24),
+          Center(
+            child: Column(
+              children: [
+                const Text(
+                  "Hello Welcome Back",
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontFamily: 'Roboto',
+                    fontSize: 26,
+                    fontWeight: FontWeight.w500,
+                  ),
+                ),
+                const SizedBox(height: 16),
+                const SizedBox(
+                  width: 150,
+                  height: 50,
+                  child: Text(
+                    "Welcome back please sign in again",
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      color: Colors.white38,
+                      fontFamily: 'Roboto',
+                      fontSize: 14,
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 50),
+                Container(
+                  padding: const EdgeInsets.all(44.0),
+                  child: Column(
+                    children: [
+                      TextField(
+                        style: TextStyle(color: Colors.white),
+                        decoration: InputDecoration(
+                          label: Text(
+                            "Email",
+                            style: TextStyle(color: Colors.white),
+                          ),
+                          hintText: "Endah FN",
+                          hintStyle: const TextStyle(
+                            fontSize: 14,
+                            color: Colors.white,
+                          ),
+                          prefixIcon: const Icon(
+                            Icons.email,
+                            color: Color(0xffC4C4C4),
+                          ),
+                          enabledBorder: UnderlineInputBorder(
+                            borderSide: BorderSide(color: Colors.white12),
+                          ),
+                          focusedBorder: UnderlineInputBorder(
+                            borderSide: BorderSide(
+                              color: Colors.white54,
+                              width: 2,
+                            ),
+                          ),
+                        ),
+                      ),
+                      const SizedBox(height: 12),
+                      TextField(
+                        obscureText: _obscureText,
+                        style: TextStyle(color: Colors.white),
+                        decoration: InputDecoration(
+                          label: Text(
+                            "Password",
+                            style: TextStyle(color: Colors.white),
+                          ),
+                          hintText: "Masukkan Kata Sandi",
+                          hintStyle: const TextStyle(
+                            fontSize: 14,
+                            color: Colors.white,
+                          ),
+                          suffixIcon: IconButton(
+                            icon: Icon(
+                              _obscureText
+                                  ? Icons.visibility_off
+                                  : Icons.visibility,
+                              color: Color(0xffC4C4C4),
+                            ),
+                            onPressed: () {
+                              setState(() {
+                                _obscureText = !_obscureText;
+                              });
+                            },
+                          ),
+                          prefixIcon: const Icon(
+                            Icons.lock,
+                            color: Color(0xffC4C4C4),
+                          ),
+                          enabledBorder: UnderlineInputBorder(
+                            borderSide: BorderSide(color: Colors.white12),
+                          ),
+                          focusedBorder: UnderlineInputBorder(
+                            borderSide: BorderSide(
+                              color: Colors.white54,
+                              width: 2,
+                            ),
+                          ),
+                        ),
+                      ),
+                      SizedBox(height: 35),
+                      Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 12.0),
+                        child: ElevatedButton.icon(
+                          onPressed: () {
+                            setState(() {});
+                          },
+                          label: Text(
+                            'Login',
+                            style: TextStyle(
+                              color: Colors.black,
+                              fontFamily: 'Roboto',
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          style: ElevatedButton.styleFrom(
+                            backgroundColor: Colors.white,
+                            foregroundColor: Colors.white,
+                            minimumSize: Size(double.infinity, 60),
+                            textStyle: TextStyle(fontSize: 16),
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(32),
+                            ),
+                          ),
+                        ),
+                      ),
+                      SizedBox(height: 48),
+                      Row(
+                        children: [
+                          Expanded(child: Divider(color: Colors.white12)),
+                          Padding(
+                            padding: EdgeInsets.symmetric(horizontal: 10),
+                            child: Text(
+                              'or',
+                              style: TextStyle(
+                                fontFamily: 'Roboto',
+                                fontSize: 12,
+                                fontWeight: FontWeight.normal,
+                                color: Colors.white,
+                              ),
+                            ),
+                          ),
+                          Expanded(child: Divider(color: Colors.white12)),
+                        ],
+                      ),
+                      SizedBox(height: 21),
+                      ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: Color(0xff11325C),
+                          foregroundColor: Colors.white,
+                          minimumSize: Size(double.infinity, 50),
+                          textStyle: TextStyle(fontSize: 16),
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(22),
+                          ),
+                        ),
+                        onPressed: () {},
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Image.asset(
+                              "assets/images/icon_facebook.png",
+                              height: 16,
+                              width: 16,
+                            ),
+                            SizedBox(width: 6),
+                            Text(
+                              "Facebook",
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontFamily: 'Roboto',
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                      SizedBox(height: 12),
+                      ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: Color(0xff11325C),
+                          foregroundColor: Colors.white,
+                          minimumSize: Size(double.infinity, 50),
+                          textStyle: TextStyle(fontSize: 16),
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(22),
+                          ),
+                        ),
+                        onPressed: () {},
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Image.asset(
+                              "assets/images/icon_google.png",
+                              height: 16,
+                              width: 16,
+                            ),
+                            SizedBox(width: 6),
+                            Text(
+                              "Gmail",
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontFamily: 'Roboto',
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                      SizedBox(height: 16),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Text(
+                            "Already have an account ?",
+                            style: TextStyle(
+                              fontSize: 12,
+                              fontWeight: FontWeight.w500,
+                              color: Colors.white,
+                            ),
+                          ),
+                          TextButton(
+                            onPressed: () {},
+                            child: Text(
+                              "Sigh In",
+                              style: TextStyle(
+                                color: Color(0xff3C7EEE),
+                                fontSize: 14,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
